43e7de3aa99a539f2386ca7f9bca84fdca2ae2ea
kausha <kausha@3five8.com>
2016-01-06 19:15:51 +0530

    css

--- src/index.php
+++ src/index.php
@@ -5,4 +5,4 @@
  * Date: 6/1/16
  * Time: 5:15 PM
  */
-echo 'kausha';
+
---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by kausha on 6/1/16.
+ */
+var app = angular.module('loanApp',[]).controller('main_controller',['$scope','$window', function($scope, $window){
+
+}]);
+
